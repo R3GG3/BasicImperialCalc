42596af78cf876b7b8b94de9cdda2656577f35c7
R3GG3 <konrad.soczi@gmail.com>
2021-03-14 15:49:20 +0100

    Small Update

--- __init__.py
+++ __init__.py
@@ -16,14 +16,16 @@ def home():
             variables.append(request.form["kilometer"])
             variables.append(request.form["meter"])
             variables.append(request.form["celsius"])
+            variables.append(request.form["kilogram"])
             return redirect(url_for("results", state=0))
         
         except:
             #STEP 2
             variables.clear()
             variables.append(request.form["mile"])
-            variables.append(request.form["foot"])
+            variables.append(request.form["feet"])
             variables.append(request.form["fahrenheit"])
+            variables.append(request.form["funt"])
             return redirect(url_for("results", state=1))
     return render_template("index.html")
 
@@ -32,24 +34,28 @@ def home():
 def results(state):
     global variables
 
+    for x in variables:
+        if x.find(",") != -1:
+            variables[variables.index(x)] = variables[variables.index(x)].replace(",", ".")
+    
     if state == "0":
         #STEP 3
         finish = {
             "mile": float(variables[0])/1.61,
-            "foot": float(variables[1])*3.28,
-            "fahrenheit": 32.00+float(variables[2])*1.80
+            "feet": float(variables[1])*3.28,
+            "fahrenheit": 32.00+float(variables[2])*1.80,
+            "funt": float(variables[3])/0.45
         }
 
     if state == "1":
         #It's here if somebody would write "," instead of "."
-        if variables[2].find(",") != -1:
-            variables[2] = variables[2].replace(",", ".")
         
         #STEP 4
         finish = {
             "kilometer": float(variables[0])*1.61,
             "meter": float(variables[1])/3.28,
-            "celsius": round((float(variables[2])-32)/1.80)
+            "celsius": round((float(variables[2])-32)/1.80),
+            "kilogram": float(variables[3])*0.45
         }
     return render_template("results.html", old=variables, variables=finish)
 
